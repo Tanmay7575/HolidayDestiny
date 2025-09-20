a368c81a0d454873b6a64b9c2cb190324921dcff
khedekar tanmay <khedekartanmay75@gmail.com>
2025-09-20 10:19:28 +0530

    Update AppContext.tsx

--- frontend/src/contexts/AppContext.tsx
+++ frontend/src/contexts/AppContext.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import { Toast } from "../components/toast";
 import { useQuery } from "@tanstack/react-query";
 import * as apiClient from '../api-client'
