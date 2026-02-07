6450e0bd90474f6582aaa7d3009ac041dfe083a7
Tanmay Khedekar <khedekartanmay75@gmail.com>
2026-02-07 10:07:49 +0530

    Delete tailwind.config.ts

--- tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from "tailwindcss";
-
-export default {
-  darkMode: "class", // important for manual theme toggling
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
-  plugins: [],
-} satisfies Config;
